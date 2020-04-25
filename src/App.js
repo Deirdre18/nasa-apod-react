b25db7df369b9e38bbe4f9f592c76b3fca864188
Deirdre18 <deirdreweldon18@gmail.com>
2020-04-26 00:22:41 +0100

    modified package-lock.json, package.json, src/App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+// eslint-disable-next-line
 import DateInput from "./components/DateInput";
 import Photo from "./components/Photo.js";
+import DatePicker from "react-datepicker";
 
+import "react-datepicker/dist/react-datepicker.css";
 class App extends Component {
 
   state = {
@@ -23,6 +26,12 @@ class App extends Component {
     this.getPhoto(dateFromInput);
   };
 
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
+
   getPhoto = date => {
     fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${process.env.REACT_APP_API_KEY_YT}`)
     .then(response => response.json())
@@ -31,9 +40,14 @@ class App extends Component {
 
   render() {
     return (
+
+
       <div>
         <h1>NASA's Astronomy Picture of the Day</h1>
-        <DateInput changeDate={this.changeDate} />
+          <DatePicker
+            selected={this.state.startDate}
+            onChange={this.handleChange}
+          />
         <Photo photo={this.state.photo} />
       </div>
     );
